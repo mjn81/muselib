eb92de11659531f48b49c0927370501f8b0d2be7
mjn81 <mjjnn81@gmail.com>
2022-07-20 05:06:12 +0430

    format

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -5,8 +5,7 @@ export const AuthLayout = (props: PropsWithChildren) => {
   const { children } = props;
   return (
     <main className="bg-purple-700 w-screen h-screen relative">
-      <div className="w-screen h-screen absolute z-0 top-0 right-0 opacity-40">
-      </div>
+      <div className="w-screen h-screen absolute z-0 top-0 right-0 opacity-40"></div>
 
       {children}
     </main>
